4fd9df3c029d0f9ebd196d1cc38c3b1043d512d3
valentineezeh <val.ezeh15@gmail.com>
2019-05-18 06:34:24 +0100

    Title: configure sequelize and connect it to mysql - set up and configure sequelize. - Connect sequelize to online database - Authenticate database connection.

--- src/tests/tests.js
+++ src/tests/tests.js
@@ -8,17 +8,16 @@ chai.use(chaiHttp);
 env.config();
 
 describe('check test', () => {
-
-    it('should get the base endpoint', (done) => {
-      chai
-        .request(App)
-        .get('/api')
-        .set({
-          'Content-type': 'application/json',
-        })
-        .end((err, res) => {
-          expect(res.status).to.equal(200);
-          done();
-        });
-    });
+  it('should get the base endpoint', (done) => {
+    chai
+      .request(App)
+      .get('/api')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
 });
